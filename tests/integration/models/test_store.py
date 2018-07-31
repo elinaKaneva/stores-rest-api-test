800b6b72128819e655842e6cc4837cb28e2bc528
Elina Kaneva (Circles) <elina.kaneva@circles.bz>
2018-07-31 17:43:45 +0300

    Changed 1 test.

--- tests/integration/models/test_store.py
+++ tests/integration/models/test_store.py
@@ -54,7 +54,7 @@ class TestStore(BaseTest):
 
             expected = {
                 'name': 'test',
-                'items': [{'name': 'test_item', 'price': 19.99}]
+                'items': [{'id': 1, 'name': 'test_item', 'price': 19.99}]
             }
 
             self.assertDictEqual(store.json(), expected)
